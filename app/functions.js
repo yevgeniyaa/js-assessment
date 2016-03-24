c9b6924ec4b56defd852443237764399fc436c57
yevgeniyaa <denzhishi@mail.ru>
2016-03-24 15:42:35 +0600

    add methods in numbers and functions

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,32 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+	return fn.apply(null, arr);
   },
 
   speak : function(fn, obj) {
-
+	return fn.call(obj);
   },
 
   functionFunction : function(str) {
-
+	return function (str2) {
+		return str + ", " + str2;
+	};
   },
 
   makeClosures : function(arr, fn) {
-
+	var resArr = [];
+	
+	var fun = function(arg) {
+		return function() {
+			return fn(arg);
+		};
+	};
+	
+	for (var i = 0; i < arr.length; i++) {
+		resArr.push(fun(arr[i]));
+    }
+	return resArr;
   },
 
   partial : function(fn, str1, str2) {
--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+	return (num >> (bit - 1)) & 1;
   },
 
   base10: function(str) {
-
+	 return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+	return setLength(num.toString(2), 8);
+	function setLength(num, length) {
+      var result = "";
+		while(length--)
+			result += (num >> length) & 1;    
+		return result;
+    }
   },
 
   multiply: function(a, b) {
-
+	var multiplier = 1000000;
+	a *= multiplier;
+    b *= multiplier;
+    return (a * b) / (multiplier * multiplier);
   }
 };

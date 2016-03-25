2083e12a4f034d7da6da05b732032e8691fa2bdd
yevgeniyaa <denzhishi@mail.ru>
2016-03-25 16:09:12 +0600

    complete objects

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,20 @@
 exports = (typeof window === 'undefined') ? global : window;
 
-exports.objectsAnswers =  {
-  alterContext : function(fn, obj) {
+exports.objectsAnswers = {
+	alterContext: function(fn, obj) {
+		return fn.call(obj);
+	},
 
-  },
+	alterObjects: function(constructor, greeting) {
+		constructor.prototype.greeting = greeting;
+	},
 
-  alterObjects : function(constructor, greeting) {
-
-  },
-
-  iterate : function(obj) {
-
-  }
+	iterate: function(obj) {
+		var result = [];
+		for (var key in obj) {
+			if (!obj.hasOwnProperty(key)) continue;
+			result.push(key + ": " + obj[key]);
+		}
+		return result;
+	}
 };

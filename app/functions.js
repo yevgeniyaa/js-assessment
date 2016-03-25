96fcb350de34a9541ac844385eb2d21f54cb9ee1
yevgeniyaa <denzhishi@mail.ru>
2016-03-25 11:21:04 +0600

    complete functions

--- app/functions.js
+++ app/functions.js
@@ -1,55 +1,63 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray : function(fn, arr) {
-   return fn.apply(null, arr);
- },
+  argsAsArray: function(fn, arr) {
+    return fn.apply(null, arr);
+  },
 
- speak : function(fn, obj) {
-   return fn.call(obj);
- },
+  speak: function(fn, obj) {
+    return fn.call(obj);
+  },
 
- functionFunction : function(str) {
-   return function (str2) {
-    return `${str}, ${str2}`;
-  };
-},
+  functionFunction: function(str) {
+    return function(str2) {
+      return `${str}, ${str2}`;
+    };
+  },
 
-makeClosures : function(arr, fn) {
-	var fun = function(arg) {
-		return function() {
-			return fn(arg);
-		};
-	};
-	var resArr = arr.map(fun);
-	return resArr;
-},
+  makeClosures: function(arr, fn) {
+    return arr.map(function(arg) {
+      return function() {
+        return fn(arg);
+      }
+    });
+  },
 
-partial : function(fn, str1, str2) {
-  var fun = fn.bind(null, str1, str2); 
-	return function (srt3) {
-		return fun(srt3);
-	};
-},
+  partial: function(fn, str1, str2) {
+    var fun = fn.bind(null, str1, str2);
+    return function(srt3) {
+      return fun(srt3);
+    };
+  },
 
-useArguments : function() {
-  var sum = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
+  useArguments: function() {
+    return [].reduce.call(arguments, function(a, b) {
+      return a + b;
+    })
+  },
+
+  callIt: function(fn) {
+    var args = [].slice.call(arguments, 1, arguments.length);
+    return fn.apply(null, args);
+  },
+
+  partialUsingArguments: function(fn) {
+    var args = [].slice.call(arguments, 1, arguments.length);
+    return function() {
+      totalArgs = args.concat([].slice.call(arguments));
+      return fn.apply(null, totalArgs);
+    };
+  },
+
+  curryIt: function(fn) {
+    return function(arg1) {
+      var partFn1 = fn.bind(null, arg1);
+      return function(arg2) {
+        var partFn2 = partFn1.bind(null, arg2);
+        return function(arg3) {
+          return partFn2(arg3);
+        }
+      };
+    };
   }
-  return sum;
-},
-
-callIt : function(fn) {
-  var args = [].slice.call(arguments, 1, arguments.length);
-  return fn.apply(null, args);
-},
-
-partialUsingArguments : function(fn) {
-
-},
-
-curryIt : function(fn) {
-
-}
 };
--- app/numbers.js
+++ app/numbers.js
@@ -2,20 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-	return (num >> (bit - 1)) & 1;
+    return (num >> (bit - 1)) & 1;
   },
 
   base10: function(str) {
-	 return parseInt(str, 2);
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-	var str = num.toString(2);
-	return new Array(9 - str.length).join('0') + str;
+    var str = num.toString(2);
+    return new Array(9 - str.length).join('0') + str;
   },
 
   multiply: function(a, b) {
-	var res = a * b;
-	return Number(res.toPrecision(6));
+    var res = a * b;
+    return Number(res.toPrecision(6));
   }
 };

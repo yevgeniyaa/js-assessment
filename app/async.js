d4c0c9688358e6650cbe0cf31777b58219ae278f
yevgeniyaa <denzhishi@mail.ru>
2016-04-06 15:54:05 +0600

    fix async function

--- app/async.js
+++ app/async.js
@@ -5,33 +5,38 @@ exports.asyncAnswers = {
 		return new Promise(function(fulfill, reject) {
 			setTimeout(function() {
 				fulfill(value);
-			}, 100);
+			}, 10);
 		});
 	},
 
 	manipulateRemoteData: function(url) {
 		function getDataFromServer(url) {
-			return new Promise(function(fulfill, reject) {
+			return new Promise((resolve, reject) => {
 				var xhr = new XMLHttpRequest();
-				xhr.open('GET', url, false);
-				xhr.send();
-				if (xhr.status != 200) {
-					reject(xhr.status + ': ' + xhr.statusText);
-				} else {
-					fulfill(JSON.parse(xhr.responseText));
+				xhr.open('GET', url, true);
+
+				xhr.onload = function() {
+					if (this.status == 200) {
+						resolve(JSON.parse(this.response));
+					} else {
+						var error = new Error(this.statusText);
+						error.code = this.status;
+						reject(error);
+					}
+				};
+
+				xhr.onerror = function() {
+					reject(new Error('Network error'));
 				}
+
+				xhr.send();
 			});
 		}
 
-		return new Promise(function(fulfill, reject) {
-			getDataFromServer(url).then(function(data) {
-				var names = data.people.map(function(person) {
-					return person.name;
-				});
-				fulfill(names.sort());
-			}, function(err) {
-				reject(err);
-			});
-		});
+		return getDataFromServer(url)
+			.then(
+				result => result.people.map(person => person.name).sort(),
+				error => `Rejected: ${error}`
+			);
 	}
 };
